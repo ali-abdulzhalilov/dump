9633890164ea3719f84d42631fdc0109f7765820
clueless-magician <clueless.magician@gmail.com>
2017-07-07 20:08:30 +0300

    my little virtual machine
    i made it, and i like it

---
+++ vm/vm/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace vm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.regBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(100, 209);
+            this.textBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.run_Click);
+            // 
+            // regBox
+            // 
+            this.regBox.Location = new System.Drawing.Point(118, 12);
+            this.regBox.Multiline = true;
+            this.regBox.Name = "regBox";
+            this.regBox.ReadOnly = true;
+            this.regBox.Size = new System.Drawing.Size(100, 134);
+            this.regBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.regBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox regBox;
+    }
+}
+
---
+++ vm/vm/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vm
+{
+    public partial class Form1 : Form
+    {
+        VM vm;
+        Dictionary<string, int> codes = new Dictionary<string, int>()
+        {
+            {"ADD",   10},
+            {"SUB",   11},
+            {"MUL",   12},
+            {"DIV",   13},
+
+            {"MOV",   20},
+            {"LOAD",  21},
+            {"STORE", 22},
+            {"SET",   23},
+
+            {"JMP",   30},
+            {"CMP",   31},
+            {"JOP",   32},
+            {"JOM",   33},
+            {"JOE",   34},
+
+            {"HALT",  40},
+            {"SLEEP", 41},
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            vm = new VM();
+        }
+
+        private void run_Click(object sender, EventArgs e)
+        {
+            string[] text = textBox.Text.Split(' ', '\n');
+            int[] code = interpret(text);
+
+            vm.run(-1, code);
+
+            regBox.Text = "";
+            int[] regs = vm.getReg();
+            for (int i = 0; i < regs.Length; i++)
+                regBox.Text += regs[i] + " ";
+        }
+
+        private int[] interpret(string[] words)
+        {
+            int[] res = new int[words.Length];
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (codes.ContainsKey(words[i]))
+                    res[i] = codes[words[i]];
+                else if (int.TryParse(words[i], out res[i]))
+                    res[i] = int.Parse(words[i]);
+            }
+
+            return res;
+        }
+
+    }
+
+    class VM
+    {
+        int pc; //program counter
+        int[] reg = new int[6];
+        int[] code;
+        int cmp;
+        bool ex;
+
+        public VM()
+        {
+            for (int i = 0; i < this.reg.Length; i++)
+                this.reg[i] = 0;
+            this.cmp = 0;
+        }
+
+        public int[] getReg()
+        {
+            return reg;
+        }
+
+        public void run(int pc, int[] code)
+        {
+            this.pc = pc;
+            this.code = code;
+            for (int i = 0; i < this.reg.Length; i++)
+                this.reg[i] = 0;
+            ex = true;
+
+            while (ex)
+            {
+                execute(ncode());
+            }
+        }
+
+        int ncode()
+        {
+            pc++;
+            if (pc >= code.Length) return 40;
+            return code[pc];
+        }
+
+        void execute(int word)
+        {
+            int regA, regB, line, num;
+
+            switch (word)
+            {
+                case 10: //add
+                    regA = ncode();
+                    regB = ncode();
+                    reg[regA] = reg[regA] + reg[regB];
+                    break;
+                case 11: //sub
+                    regA = ncode();
+                    regB = ncode();
+                    reg[regA] = reg[regA] - reg[regB];
+                    break;
+                case 12: //mul
+                    regA = ncode();
+                    regB = ncode();
+                    reg[regA] = reg[regA] * reg[regB];
+                    break;
+                case 13: //div
+                    regA = ncode();
+                    regB = ncode();
+                    reg[regA] = reg[regA] / reg[regB];
+                    break;
+
+                case 20: //mov
+                    regA = ncode();
+                    regB = ncode();
+                    reg[regB] = reg[regA];
+                    break;
+                case 21: //load
+                    line = ncode();
+                    regA = ncode();
+                    reg[regA] = code[line];
+                    break;
+                case 22: //store
+                    regA = ncode();
+                    line = ncode();
+                    code[line] = reg[regA];
+                    break;
+                case 23: //set
+                    regA = ncode();
+                    num = ncode();
+                    reg[regA] = num;
+                    break;
+
+                case 30: //jmp
+                    line = ncode();
+                    pc = line - 1;
+                    break;
+                case 31: //cmp
+                    regA = ncode();
+                    regB = ncode();
+                    if (reg[regA] > reg[regB]) cmp = 1;
+                    else if (reg[regA] == reg[regB]) cmp = 0;
+                    else cmp = -1;
+                    break;
+                case 32: //jop
+                    line = ncode();
+                    if (cmp == 1)
+                        pc = line - 1;
+                    break;
+                case 33: //jom
+                    line = ncode();
+                    if (cmp == -1)
+                        pc = line - 1;
+                    break;
+                case 34: //joe
+                    line = ncode();
+                    if (cmp == 0)
+                        pc = line - 1;
+                    break;
+
+                case 40: //halt
+                    ex = false;
+                    break;
+                case 41: //sleep
+                    regA = ncode();
+                    System.Threading.Thread.Sleep(reg[regA] * 1000);
+                    break;
+            }
+        }
+    }
+}
