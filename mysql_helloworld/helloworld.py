ed1dc7c890562b7a27ea4a3675c6a65e191d9ffe
mark-solo <clueless.magician@gmail.com>
2018-10-16 20:41:31 +0300

    mysql import

--- mysql_helloworld/helloworld.py
+++ mysql_helloworld/helloworld.py
@@ -1,4 +1,9 @@
-def pew():
-	return "meow"
-	
-print(pew())
+import MySQLdb
+
+conn=MySQLdb.connect(host="localhost", user="root", passwd="13245", db="test")
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM users")
+
+row = cursor.fetchone()
+print(row)
